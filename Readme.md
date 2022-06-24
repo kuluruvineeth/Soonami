555bc0ed97b61c7fc473e79979ee03d7823d6220
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-06-24 19:39:38 +0530

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# A Simple Application Demonstrating making HTTP calls over the internet
+
+- Enabling uses-permission INTERNET
+
+## Screenshots
+![img1](https://github.com/kuluruvineeth/Soonami/blob/master/Screenshots/img.png)
---
+++ app/src/main/java/com/agririze/soonami/Event.java
@@ -0,0 +1,13 @@
+package com.agririze.soonami;
+
+public class Event {
+    public final String title;
+    public final long time;
+    public final int tsunamiAlert;
+
+    public Event(String title, long time, int tsunamiAlert) {
+        this.title = title;
+        this.time = time;
+        this.tsunamiAlert = tsunamiAlert;
+    }
+}
